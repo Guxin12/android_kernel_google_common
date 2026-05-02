1645a8c106a0775eb3cc0a9c104e1dac2d1c344b
Guxin12 <2452148391@qq.com>
2026-05-02 17:08:45 +0800

    Pull the latest commit of android16-6.12-lts
    Signed-off-by: Guxin12 <2452148391@qq.com>

---
+++ arch/arm64/include/asm/tlbbatch.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ARCH_ARM64_TLBBATCH_H
+#define _ARCH_ARM64_TLBBATCH_H
+
+#include <linux/cpumask.h>
+
+struct arch_tlbflush_unmap_batch {
+};
+
+#endif /* _ARCH_ARM64_TLBBATCH_H */
